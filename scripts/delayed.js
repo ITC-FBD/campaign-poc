82dbf24bd9b5389728ab3e2db30b7883f2f13a8b
VandanaBV <vandana.bv@axeno.co>
2024-05-13 12:43:32 +0530

    fixed build issues

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -9,7 +9,7 @@ const icons = document.querySelector('main').querySelectorAll('span.icon');
 icons.forEach((span) => {
   const iconName = Array.from(span.classList).find((c) => c.startsWith('icon-')).substring(5);
   const rating = span.parentElement.parentElement.nextElementSibling.innerText;
-  if (iconName == 'star-rating') {
-   span.style = '--actual-rating:' + rating + ';';
+  if (iconName === 'star-rating') {
+    span.style = `--actual-rating: ${rating};`;
   }
 });
